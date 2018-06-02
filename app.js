8e22ee7aa4094de5f380a1fcad33e4f62823b65a
EriksonG <eriksonlove@gmail.com>
2018-06-02 22:28:41 +0100

    Assets e Login

--- app.js
+++ app.js
@@ -10,11 +10,24 @@ var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static('public'));
+
+/*
 app.get('/', function (req, res) {
     res.status(200).send({ Fichero: 'Recibido' });
+});
+*/
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/login.html'));
 });
 
 server.listen(port, () => {
